f752cc7d89236e28d2d50cbbcbdbd0d4c8b45693
ManuAzevedo <manuely.neves@estudante.ifms.edu.br>
2023-09-25 08:26:08 -0400

    install

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,11 +1,51 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
+type FilmeType = {
+    id:number,
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
+
 export default function Main() {
-    let textodigitado = 'Barbie'
-    function TrataTexto(){
-        console.log("Digitado")
+    //let textodigitado = 'Barbie'
+    //Hooks são funções do React que ajudam a gente a fazer tarefas
+    //específicas
+    const [texto,setTexto]=useState("")
+
+    const filmes:FilmeType[] = [
+        {
+            id:1,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:2,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser.',
+            imagem:'/KEN.png'
+        },
+        {
+            id:3,
+            titulo:'Filme Barbie',
+            sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+            imagem:'/boneca.jpg'
+        },
+        {
+            id:5,
+            titulo:'Barbie',
+            sinopse:"Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+            imagem:'/barbie.png'
+        },
+    ]
+
+    //O parâmetro "e" da minha função será o meu evento que ocorreu
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        //console.log(e.target.value)
         //Como eu faço para mudar o texto para "TERE"
-        textodigitado = "TERE"
+        setTexto(e.target.value)
     }
     return (
         <>
@@ -15,10 +55,26 @@ export default function Main() {
                        className='botao_pesquisa'
                        placeholder='Pesquise um Filme'
                        onChange={TrataTexto} />
-                <p>Digitado: {textodigitado} </p>
+                {texto && <p>Resultados Para: {texto} </p>}
             </div>
             <main className="content-main">
-                <Filme titulo='Barbie'
+                {/* 
+                    Use algo do vetor para tentar criar os filmes 
+                */}
+                {
+                    filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map(
+                        (filme)=>
+                            <Filme 
+                                key={filme.id}
+                                sinopse={filme.sinopse}
+                                titulo={filme.titulo}
+                                imagem={filme.imagem}
+                            />
+                    )
+                }
+
+                
+                {/* <Filme titulo='Barbie'
                     sinopse='Depois de ser expulsa da 
                    Barbieland por ser uma boneca de aparência 
                    menos do que perfeita, Barbie parte para o 
@@ -71,7 +127,7 @@ export default function Main() {
                    menos do que perfeita, Barbie parte para o 
                    mundo humano em busca da verdadeira felicidade.'
                     imagem='/boneca.jpg'
-                />
+                /> */}
 
             </main>
         </>
